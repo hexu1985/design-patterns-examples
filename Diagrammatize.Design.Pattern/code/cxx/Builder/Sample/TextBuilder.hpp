5f9cfefa6b2655b056e25fdd3dcc51ae49f83a1d
hexu <hexu_1985@sina.com>
2022-12-21 15:30:51 +0800

    add Diagrammatize.Design.Pattern/code/cxx/Builder/Sample

---
+++ Diagrammatize.Design.Pattern/code/cxx/Builder/Sample/TextBuilder.hpp
@@ -0,0 +1,19 @@
+#pragma once
+
+#include "Builder.hpp"
+#include <sstream>
+
+class TextBuilder : public Builder {
+private:
+    std::ostringstream buffer;      // 文档内容保存在该字段中
+
+public:
+    ~TextBuilder() override;
+
+    void makeTitle(const std::string& title) override;
+    void makeString(const std::string& str) override;
+    void makeItems(const std::vector<std::string>& items) override;
+    void close() override;
+
+    std::string getResult();
+};
