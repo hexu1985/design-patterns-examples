bd427ead08958eea2c30c28f30144c0ce086b5fe
hexu <hexu_1985@sina.com>
2022-12-22 18:38:43 +0800

    add Diagrammatize.Design.Pattern/code/python/Prototype/Sample

---
+++ Diagrammatize.Design.Pattern/code/python/Prototype/Sample/Main.py
@@ -0,0 +1,22 @@
+from framework.Manager import Manager
+from framework.Product import Product
+from UnderlinePen import UnderlinePen
+from MessageBox import MessageBox
+
+def main():
+    # 准备
+    manager = new Manager()
+    upen = new UnderlinePen('~')
+    mbox = new MessageBox('*')
+    sbox = new MessageBox('/')
+    manager.register("strong message", upen)
+    manager.register("warning box", mbox)
+    manager.register("slash box", sbox)
+    
+    # 生成
+    p1 = manager.create("strong message")
+    p1.use("Hello, world.")
+    p2 = manager.create("warning box")
+    p2.use("Hello, world.")
+    p3 = manager.create("slash box")
+    p3.use("Hello, world.")
