75369fbc17862f435da86f1001d056044cbdd178
hexu <hexu_1985@sina.com>
2022-12-22 10:30:04 +0800

    add Diagrammatize.Design.Pattern/code/python/Builder/A4/

---
+++ Diagrammatize.Design.Pattern/code/python/Builder/A4/TextBuilder.py
@@ -0,0 +1,25 @@
+
+class TextBuilder:
+    def __init__(self):
+        self.buffer = ''       # 文档内容保存在该字段中 
+
+    def makeTitle(self, title):                         # 纯文本的标题
+        self.buffer += "==============================\n"       # 装饰线
+        self.buffer += "『" + title + "』\n"                    # 为标题加上『』
+        self.buffer += "\n"                                     # 换行
+
+
+    def makeString(self, str_):                         # 纯文本的字符串
+        self.buffer += "■" + str_ + "\n"                        # 为字符串添加■
+        self.buffer += "\n"                                     # 换行
+
+    def makeItems(self, items):                         # 纯文本的条目
+        for i in range(len(items)):
+            self.buffer += "　・" + items[i] + "\n"             # 为条目添加・
+        self.buffer += "\n"                                     # 换行
+
+    def close(self):                                    # 完成文档
+        self.buffer += "==============================\n"       # 装饰线
+
+    def getResult(self):                                # 完成后的文档
+        return self.buffer
