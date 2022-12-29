da5e897926ac1b12802170119f7c86e4e5d79278
hexu <hexu_1985@sina.com>
2022-12-29 15:13:21 +0800

    update Diagrammatize.Design.Pattern/code/cxx/Visitor/Sample

--- Diagrammatize.Design.Pattern/code/cxx/Visitor/Sample/Entry.cpp
+++ Diagrammatize.Design.Pattern/code/cxx/Visitor/Sample/Entry.cpp
@@ -3,7 +3,11 @@
 
 Entry::~Entry() {}
 
+std::string Entry::toString() {
+    return getName() + " (" + std::to_string(getSize()) + ")";
+}
+
 std::ostream& operator<<(std::ostream& out, Entry& entry) {
-    out << entry.getName() << " (" << entry.getSize() << ")";
+    out << entry.toString();
     return out;
 }
--- Diagrammatize.Design.Pattern/code/cxx/Visitor/Sample/Entry.hpp
+++ Diagrammatize.Design.Pattern/code/cxx/Visitor/Sample/Entry.hpp
@@ -8,8 +8,9 @@ class Entry : public Element {
 public:
     virtual ~Entry();
 
-    virtual std::string getName() = 0;     // 获取名字
-    virtual int getSize() = 0;             // 获取大小
+    virtual std::string getName() = 0;      // 获取名字
+    virtual int getSize() = 0;              // 获取大小
+    std::string toString();                 // 显示字符串
 };
 
 std::ostream& operator<<(std::ostream& out, Entry& entry);
