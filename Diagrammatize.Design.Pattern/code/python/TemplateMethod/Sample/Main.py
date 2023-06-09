5b45def2ff59ea468a203917ace66941e3c27e96
hexu <hexu_1985@sina.com>
2023-06-09 16:12:31 +0800

    update Diagrammatize.Design.Pattern/code/*/TemplateMethod/

--- Diagrammatize.Design.Pattern/code/python/TemplateMethod/Sample/Main.py
+++ Diagrammatize.Design.Pattern/code/python/TemplateMethod/Sample/Main.py
@@ -2,12 +2,12 @@ from CharDisplay import CharDisplay
 from StringDisplay import StringDisplay
 
 def main():
-    d1 = CharDisplay('H');                  # 生成一个持有'H'的CharDisplay类的实例 
-    d2 = StringDisplay("Hello, world.");    # 生成一个持有"Hello, world."的StringDisplay类的实例 
-    d3 = StringDisplay("你好，世界。");     # 生成一个持有"你好，世界。"的StringDisplay类的实例 
-    d1.display();                           # 由于d1、d2和d3都是AbstractDisplay类的子类
-    d2.display();                           # 可以调用继承的display方法
-    d3.display();                           # 实际的程序行为取决于CharDisplay类和StringDisplay类的具体实现 
+    d1 = CharDisplay('H')                    # 生成一个持有'H'的CharDisplay类的实例 
+    d2 = StringDisplay("Hello, world.")      # 生成一个持有"Hello, world."的StringDisplay类的实例 
+    d3 = StringDisplay("See you tomorrow.")  # 生成一个持有"See you tomorrow."的StringDisplay类的实例 
+    d1.display()                             # 由于d1、d2和d3都是AbstractDisplay类的子类
+    d2.display()                             # 可以调用继承的display方法
+    d3.display()                             # 实际的程序行为取决于CharDisplay类和StringDisplay类的具体实现 
 
 if __name__ == "__main__":
     main()
