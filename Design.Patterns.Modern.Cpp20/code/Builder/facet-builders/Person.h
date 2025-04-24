4b2e2c304794342e86674b13914563ebc43620a8
hexu <hexu_1985@sina.com>
2025-04-24 23:16:16 +0800

    add Design.Patterns.Modern.Cpp20/code/Builder

---
+++ Design.Patterns.Modern.Cpp20/code/Builder/facet-builders/Person.h
@@ -0,0 +1,57 @@
+#pragma once
+#include <string>
+using namespace std;
+using namespace std::string_literals;
+class PersonBuilder;
+
+class Person
+{
+    // address
+    string street_address, post_code, city;
+
+    // employment
+    string company_name, position;
+    int annual_income = 0;
+
+    Person() {}
+public:
+    static PersonBuilder create();
+
+    Person(Person&& other)
+        : street_address{move(other.street_address)},
+        post_code{move(other.post_code)},
+        city{move(other.city)},
+        company_name{move(other.company_name)},
+        position{move(other.position)},
+        annual_income{other.annual_income}
+    {
+    }
+
+    Person& operator=(Person&& other)
+    {
+        if (this == &other)
+            return *this;
+        street_address = move(other.street_address);
+        post_code = move(other.post_code);
+        city = move(other.city);
+        company_name = move(other.company_name);
+        position = move(other.position);
+        annual_income = other.annual_income;
+        return *this;
+    }
+
+    friend ostream& operator<<(ostream& os, const Person& obj)
+    {
+        return os
+            << "street_address: "s << obj.street_address
+            << " post_code: "s << obj.post_code
+            << " city: "s << obj.city
+            << " company_name: "s << obj.company_name
+            << " position: "s << obj.position
+            << " annual_income: "s << std::to_string(obj.annual_income);
+    }
+
+    friend class PersonBuilder;
+    friend class PersonAddressBuilder;
+    friend class PersonJobBuilder;
+};
