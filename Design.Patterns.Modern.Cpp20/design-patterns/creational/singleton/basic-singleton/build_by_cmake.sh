937c3dbcd556add81595c0e35f242b5abcb8c6b4
hexu <hexu_1985@sina.com>
2025-08-13 16:36:13 +0800

    add cmake file for design-patterns/creational/singleton/basic-singleton

---
+++ Design.Patterns.Modern.Cpp20/design-patterns/creational/singleton/basic-singleton/build_by_cmake.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+current_dir=$( cd $(dirname ${BASH_SOURCE[0]}) && pwd )
+src_dir=${current_dir}
+build_dir=${current_dir}/build
+
+cmake -S ${src_dir} -B ${build_dir} \
+    -DBoost_DIR=~/local/boost/lib/cmake/Boost-1.88.0 \
+    -DCMAKE_BUILD_TYPE=Debug
+cmake --build ${build_dir} -j 4
+
