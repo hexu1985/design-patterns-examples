5afafb3ea7463fa661eb12106b9d09e5e30948ba
hexu <hexu_1985@sina.com>
2026-03-06 14:14:07 +0800

    add Design.Patterns.Modern.Cpp20/code/Bridge

---
+++ Design.Patterns.Modern.Cpp20/code/Bridge/pimpl_idiom/Person.hpp
@@ -0,0 +1,17 @@
+#pragma once
+
+#include <string>
+
+class Person {
+public:
+    std::string name;
+
+    // we hide the actual implementation to whoever is using this header file
+    class PersonImpl;
+    PersonImpl *impl;
+
+    Person();
+    ~Person();
+
+    void greet();
+};
