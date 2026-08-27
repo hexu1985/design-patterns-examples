d774b94f06337231f3930807c6e78518d599b109
hexu <hexu_1985@sina.com>
2026-08-27 17:15:44 +0800

    update Diagrammatize.Java.Mutlithreading.Design.Patterns/cxx/GuardedSuspension/A3-6

--- Diagrammatize.Java.Mutlithreading.Design.Patterns/cxx/GuardedSuspension/A3-6/main.cpp
+++ Diagrammatize.Java.Mutlithreading.Design.Patterns/cxx/GuardedSuspension/A3-6/main.cpp
@@ -6,19 +6,19 @@
 int main() {
     RequestQueue requestQueue;
     
-    ClientThread client(requestQueue, "Alice", 3141592L);
-    ServerThread server(requestQueue, "Bobby", 6535897L);
+    ClientThread alice(requestQueue, "Alice", 3141592L);
+    ServerThread bobby(requestQueue, "Bobby", 6535897L);
     
-    client.start();
-    server.start();
+    alice.start();
+    bobby.start();
     
     // 等待约10秒
     std::this_thread::sleep_for(std::chrono::seconds(10));
 
     // 调用interrupt方法
     std::cout << "***** calling interrupt *****" << std::endl;
-    client.interrupt();
-    server.interrupt();
+    alice.interrupt();
+    bobby.interrupt();
 
     return 0;
 }
