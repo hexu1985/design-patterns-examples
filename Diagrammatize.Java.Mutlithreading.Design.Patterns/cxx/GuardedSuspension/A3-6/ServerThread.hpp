e10c0eef1a78c218db436cf444c85c7781d52698
hexu <hexu_1985@sina.com>
2026-08-27 14:53:01 +0800

    add Diagrammatize.Java.Mutlithreading.Design.Patterns/cxx/GuardedSuspension/A3-6

---
+++ Diagrammatize.Java.Mutlithreading.Design.Patterns/cxx/GuardedSuspension/A3-6/ServerThread.hpp
@@ -0,0 +1,47 @@
+#pragma once
+
+#include "Request.hpp"
+#include "RequestQueue.hpp"
+
+#include <thread>
+#include <random>
+#include <chrono>
+#include <string>
+#include <iostream>
+
+class ServerThread {
+private:
+    RequestQueue& requestQueue;
+    std::string threadName;
+    std::thread thread;
+    std::mt19937 random;
+    
+public:
+    ServerThread(RequestQueue& requestQueue, const std::string& name, long seed)
+        : requestQueue(requestQueue), threadName(name), random(seed) {}
+
+    ~ServerThread() {
+        join();
+    }
+    
+    void run() {
+        for (int i = 0; i < 100; i++) {
+            Request request = requestQueue.getRequest();
+            std::cout << threadName << " handles  " << request.toString() << std::endl;
+            
+            std::uniform_int_distribution<int> dist(0, 999);
+            int sleepTime = dist(random);
+            std::this_thread::sleep_for(std::chrono::milliseconds(sleepTime));
+        }
+    }
+    
+    void start() {
+        thread = std::thread([this]() { this->run(); });
+    }
+
+    void join() {
+        if (thread.joinable()) {
+            thread.join();
+        }
+    }
+};
